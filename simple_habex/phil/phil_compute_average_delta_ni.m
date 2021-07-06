b012f0e59f6117a5f417751953f025b6b32f6aa5
Phillip Poon <phillip.poon@jpl.nasa.gov>
2021-07-06 08:21:01 -0700

    Changed return_path_for_noise_trials.m and phil_compute_average_delta_ni.m

--- simple_habex/phil/phil_compute_average_delta_ni.m
+++ simple_habex/phil/phil_compute_average_delta_ni.m
@@ -1,34 +1,63 @@
-function [data] = phil_compute_average_delta_ni(SeriesVec, TrialNumVec)
+function [data] = phil_compute_average_delta_ni(SeriesVec, TrialNumVec, NoiseType)
 
 
 if length(SeriesVec) == 1
-   SeriesVec = SeriesVec*ones(size(TrialNumVec)); 
+    SeriesVec = SeriesVec*ones(size(TrialNumVec));
 end
 
-    
+
 delta_ni_list = [];
-    
-amplitude_list = [];
+
+switch NoiseType
+    case 'periodic'
+        amplitude_list = [];
+    case 'brownian'
+        pv_list = [];
+    otherwise
+        error('Noise Type not found')
+end
+
 
 for fi = 1:length(TrialNumVec)
-   
-    path = return_path_for_noise_trials(SeriesVec, TrialNumVec, fi);
+    
+    path = return_path_for_noise_trials(SeriesVec, TrialNumVec, NoiseType, fi);
     
     load(path.full)
     
     delta_ni_list = [delta_ni_list run.delta_ni];
     
-    amplitude_list = [amplitude_list run.amplitude];
+    switch NoiseType
+        case 'periodic'
+            amplitude_list = [amplitude_list run.amplitude];
+        case 'brownian'
+            pv_list = [pv_list run.des_pv];
+    end
+    
+    
     
 end
 
-if length(unique(amplitude_list)) > 1
+
+switch NoiseType
     
-    error('amplitude_list has more than 1 unqiue value')
+    case 'periodic'
+        
+        if length(unique(amplitude_list)) > 1
+            error('amplitude_list has more than 1 unqiue value')
+        end
+        data.unique_amplitude_values = unique(amplitude_list);
+        
+    case 'brownian'
+        
+        data.unique_pv_values = unique(pv_list);
+        
+        
 end
 
 data.mean_delta_ni = mean(delta_ni_list);
 data.std_of_the_mean_delta_ni = std(delta_ni_list);
-data.unique_amplitude_values = unique(amplitude_list);
+
+
+
 
 end
--- simple_habex/phil/return_path_for_noise_trials.m
+++ simple_habex/phil/return_path_for_noise_trials.m
@@ -1,23 +1,33 @@
-function [path] = return_path_for_noise_trials(SeriesVec, TrialVec, fi)
+function [path] = return_path_for_noise_trials(SeriesVec, TrialVec, NoiseType, fi)
  
     SeriesNum = SeriesVec(fi);
     TrialNum = TrialVec(fi);
     
     label = ['Series', num2str(SeriesNum,'%04.f'), '_Trial', num2str(TrialNum,'%04.f') ''];
     
+    
+    
+    switch NoiseType
+        case 'periodic'
+            save_dir = 'noise';
+        case 'brownian'
+            save_dir = 'noise_drift';
+    end
+    
     if ismac
         
-        path.png = ['/Volumes/poon/dst_sim/proper-models/simple_habex/workspaces/noise/png/' label '/'];
-        path.ws = '/Volumes/poon/dst_sim/proper-models/simple_habex/workspaces/noise/';
+        path.png = ['/Volumes/poon/dst_sim/proper-models/simple_habex/workspaces/', save_dir,'/png/' label '/'];
+        path.ws = ['/Volumes/poon/dst_sim/proper-models/simple_habex/workspaces/', save_dir,'/'];
         
     elseif isunix && ~ismac
         
-        path.png = ['/home/poon/dst_sim/proper-models/simple_habex/workspaces/noise/png/' label '/'];
-        path.ws = '/home/poon/dst_sim/proper-models/simple_habex/workspaces/noise/';
+        path.png = ['/home/poon/dst_sim/proper-models/simple_habex/workspaces/', save_dir,'/png/' label '/'];
+        path.ws =  ['/home/poon/dst_sim/proper-models/simple_habex/workspaces/', save_dir,'/'];
     end
     
     
     path.full = [path.ws label '.mat']
+    
         
     
 end
